a7b270bfc8290bc73bd288faa93f6e38d7f45312
haha <125535033+wangba2023@users.noreply.github.com>
2024-09-12 12:18:40 +0800

    Update IP.js

--- IP.js
+++ IP.js
@@ -1,13 +1,10 @@
 if ($response.statusCode != 200) {
   $done(Null);
 }
-
 var body = $response.body;
 var obj = JSON.parse(body);
 var title = obj['country'];
 var subtitle =  obj['isp'];
 var ip = obj['query'];
 var description = "国家" + ":" + obj['country'] + ":" + obj['isp']];
-
-
 $done({title, subtitle, ip, description});
